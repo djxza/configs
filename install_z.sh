41b5b225e483e10e6ee6447a58de95c5e700e6b4
djoza <104144342+djxza@users.noreply.github.com>
2026-02-18 12:45:30 +0100

    Add installation script for Zsh and plugins

---
+++ install_z.sh
@@ -0,0 +1,11 @@
+sudo pacman -S zsh curl git
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+curl -fsSL https://raw.githubusercontent.com/djxza/configs/master/.zshrc -o ~/.zshrc
+source ~/.zshrc
